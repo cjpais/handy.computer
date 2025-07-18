38681c32ce8d0a627374c22060888db8a6c7ad38
CJ Pais <cj@cjpais.com>
2025-07-17 20:22:41 -0700

    add donate redirect for a cloudflare?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://handy.computer",
   integrations: [react(), mdx()],
-  output: "static", // This ensures static site generation
+  adapter: cloudflare(),
+  output: "hybrid",
   build: {
     format: "file",
   },
@@ -15,5 +17,8 @@ export default defineConfig({
   },
   image: {
     remotePatterns: [{ protocol: "https" }],
+  },
+  redirects: {
+    "/donate": "https://donate.stripe.com/6oU4gz8762g9790c8Vffy0j",
   },
 });
