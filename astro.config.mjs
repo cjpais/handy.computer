00f487552858855a9750002351d068753e162103
CJ Pais <cj@cjpais.com>
2025-07-17 20:26:54 -0700

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://handy.computer",
   integrations: [react(), mdx()],
-  adapter: cloudflare(),
-  output: "server",
+  output: "static",
   build: {
     format: "file",
   },
@@ -17,8 +15,5 @@ export default defineConfig({
   },
   image: {
     remotePatterns: [{ protocol: "https" }],
-  },
-  redirects: {
-    "/donate": "https://donate.stripe.com/6oU4gz8762g9790c8Vffy0j",
   },
 });
