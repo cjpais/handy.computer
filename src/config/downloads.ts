9bb87f56ff22bb6259690c4061c344bc074f8a8d
CJ Pais <cj@cjpais.com>
2026-07-01 22:01:39 +0800

    0.9.0

--- src/config/downloads.ts
+++ src/config/downloads.ts
@@ -1,4 +1,4 @@
-export const VERSION = "0.8.3";
+export const VERSION = "0.9.0";
 export const VERSION_TAG = `v${VERSION}`;
 export const GITHUB_RELEASE_BASE = `https://github.com/cjpais/Handy/releases/download/${VERSION_TAG}`;
 
